c4ecdfaa897038e08be1386c2b78c192dcdea9d1
Jakub Gedeon <jgedeon@cslab7.csnet.montana.edu>
2013-10-22 13:03:04 -0600

    Added basic bootstrap template + favicon

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!python2
 
-from flask import Flask, g
+from flask import Flask, g, render_template
 app = Flask(__name__)
 
 # Database initialization
@@ -8,17 +8,17 @@ import sqlite3
 
 @app.before_request
 def init_db():
-	g.db = sqlite3.connect('data.db')
+    g.db = sqlite3.connect('data.db')
 
 @app.teardown_request
 def close_db(exception):
-	if getattr(g, 'db', None):
-		g.db.close()
+    if getattr(g, 'db', None):
+        g.db.close()
 
 
 @app.route('/')
 def hello():
-	return 'Hello World!'
+    return render_template('layout.html')
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
