9ded60178521a2077300aa60a9b7dafead86fd20
Jakub Gedeon <jakub.gedeon@webfilings.com>
2013-11-10 01:18:37 -0700

    Added database access and config file loading

---
+++ database.py
@@ -0,0 +1,50 @@
+# Singleton for dealing with the backend
+from contextlib import contextmanager
+
+class AbstractDatabase(object):
+    def __init__(self):
+        pass
+        # Will contain table creation and fixing logic
+
+    def connection(self):
+        """ Abstract handle for getting a connection, irrelevent of backend
+        Should use with keyword, and be a context manager """
+        raise NotImplementedError("Cannot connect to abstract database; must subclass")
+
+
+import sqlite3
+class SQLiteDatabase(AbstractDatabase):
+    def __init__(self, config):
+        self.filename = config['filename']
+        super(SQLiteDatabase, self).__init__()
+
+    @contextmanager
+    def connection(self):
+        cnx = sqlite3.connect(self.filename)
+        yield cnx
+        cnx.close()
+        return
+
+
+class MySQLDatabase(AbstractDatabase):
+    def __init__(self, config):
+        from mysql.connector import Connect
+        self.Connect = Connect
+        self.params = config['params']
+        super(MySQLDatabase, self).__init__()
+
+    @contextmanager
+    def connection(self):
+        cnx = self.Connect(**self.params)
+        yield cnx
+        cnx.close()
+
+
+def get_database(config):
+    dbconfig = config['database']
+    if dbconfig['type'] == 'sqlite':
+        return SQLiteDatabase(dbconfig)
+    elif dbconfig['type'] == 'mysql':
+        return MySQLDatabase(dbconfig)
+    else:
+        raise NotImplementedError("Invalid DB type")
--- server.py
+++ server.py
@@ -1,24 +1,37 @@
 #!python2
-
 from flask import Flask, g, render_template
 app = Flask(__name__)
 
-# Database initialization
-import sqlite3
-
-@app.before_request
-def init_db():
-    g.db = sqlite3.connect('data.db')
-
-@app.teardown_request
-def close_db(exception):
-    if getattr(g, 'db', None):
-        g.db.close()
-
-
 @app.route('/')
 def hello():
-    return render_template('layout.html')
+    with app.db.connection() as cnx:
+        return render_template('layout.html')
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Parse command line arguments
+    from argparse import ArgumentParser
+    parser = ArgumentParser(
+    	description='Web server for mushroom gathering data collection',
+    	epilog='Easy config setup advice: copy and modify existing .json to config.json')
+    parser.add_argument('-c', '--config',
+        help='Load configuration from alternate file',
+        default='config.json')
+    args = parser.parse_args()
+
+    # Read config
+    import json
+    with open(args.config) as config_file:
+        config = json.loads(config_file.read())
+
+    # Database initialization
+    from database import get_database
+    app.db = get_database(config)
+
+    # Run server
+    params = {
+        'debug': config.get('debug', False),
+        'port': config.get('port', 5000)
+    }
+    app.run(**params)
