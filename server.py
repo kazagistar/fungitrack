5dbb05afa16f7aff8d3fa0930afb3903276accd9
Jakub Gedeon <jakub.gedeon@webfilings.com>
2013-11-15 19:19:24 -0700

    Database is OOP, fixed formatting, enabled sqlite compat, -r quits when done

--- server.py
+++ server.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     # Parse command line arguments
     from argparse import ArgumentParser
     parser = ArgumentParser(
-    	description='Web server for mushroom gathering data collection',
-    	epilog='Easy config setup advice: copy and modify existing .json to config.json')
+        description='Web server for mushroom gathering data collection',
+        epilog='Easy config setup advice: copy and modify existing .json to config.json')
     parser.add_argument('-c', '--config',
         help='Load configuration from alternate file',
         default='config.json')
     parser.add_argument('-r','--reload',
-                        help='Reload db from schema',
-                        action='store_true')
+        help='Reload db from schema and exit',
+        action='store_true')
     args = parser.parse_args()
 
     # Read config
@@ -29,10 +29,11 @@ if __name__ == "__main__":
         config = json.loads(config_file.read())
 
     # Database initialization
-    from database import get_database, reinit_database
+    from database import get_database
     app.db = get_database(config)
     if args.reload:
-        reinit_database(app.db)
+        app.db.reinitialize()
+        quit()
 
     # Run server
     app.secret_key = 'development key'
