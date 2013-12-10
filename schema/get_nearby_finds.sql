90378a7af821565c6d6dd53780b230e1d31b45ee
hunterlapp <hunterlapp@ubuntu.(none)>
2013-12-10 12:34:59 -0700

    fixed get_nearby_finds.sql

--- schema/get_nearby_finds.sql
+++ schema/get_nearby_finds.sql
@@ -1,2 +1,8 @@
-SELECT (Mushroom_id, Found_lat, Found_long, Found_date, Quantity, Genus, Species, Variety) FROM MUSHROOM_FINDS JOIN MUSHROOM WHERE
-ACOS(SIN(%s) * SIN(Found_lat) + COS(%s) * COS(Found_long) * COS(%s - found_long)) * 6378.1 < %s;
+SELECT MUSHROOM.Mushroom_id, Found_lat, Found_long, Found_date, Quantity, Genus, Species, Variety 
+FROM MUSHROOM_FIND JOIN MUSHROOM ON MUSHROOM.Mushroom_id = MUSHROOM_FIND.Mushroom_id
+WHERE (ACOS(SIN(%s)
+           * SIN(Found_lat) 
+           + COS(%s) 
+           * COS(Found_long) 
+           * COS(%s - Found_long)
+           ) * 6378.1) < %s;
